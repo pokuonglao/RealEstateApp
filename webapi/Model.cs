777571f10a4d22ab025af56adc64f80d546ec5f8
pokuong lao <pokuonglao123@gmail.com>
2023-10-04 09:31:53 -0700

    Add project files.

---
+++ webapi/Model.cs
@@ -0,0 +1,31 @@
+namespace webapi;
+
+public class Client
+{
+    public int ID { get; set; }
+
+    public TimeOnly Time { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public string Location { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Address { get; set; }
+
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public int Zipcode { get; set; }
+
+    public int Phone { get; set; }
+
+    public string Email { get; set; }
+
+    public string  { get; set; }
+
+}
